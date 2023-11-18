d39fd3740ca50315050e91c49d5e566b93bb42e0
Julia-Holovko777 <golovko.j27@gmail.com>
2023-11-18 20:46:08 +0200

    add 02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const getCurrentTime = function (currentTime) {
+  const seconds = currentTime.seconds;
+  localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds));
+};
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
