63d84c0772d41174ad407661f15cc2d8addadfa5
Julia-Holovko777 <golovko.j27@gmail.com>
2023-11-20 15:42:46 +0200

    add 03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+let dataForm = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {};
+const { email, message } = form.elements;
+reloadPage();
+
+function onFormInput(event) {
+  dataForm = { email: email.value, message: message.value };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(dataForm));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log({ email: email.value, message: message.value });
+  if (email.value === '' || message.value === '') {
+    return alert(`Обов'язкові поля повинні бути заповнені`);
+  }
+
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  event.currentTarget.reset();
+  dataForm = {};
+}
+function reloadPage() {
+  if (dataForm) {
+    email.value = dataForm.email || '';
+    message.value = dataForm.message || '';
+  }
+}
